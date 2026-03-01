c710f8c9c9a7762aa83ac12c0e134a7a459379a9
johnkmccann <163747271+johnkmccann@users.noreply.github.com>
2026-03-01 11:26:34 +0100

    Created Scores component for the leaderboard page

---
+++ frontend/src/pages/Scores.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Leaderboard: React.FC = () => {
+    return (
+        <div>
+            <h1>Leaderboard</h1>
+            <p>Scores will be displayed here.</p>
+        </div>
+    );
+};
+
+export default Leaderboard;
