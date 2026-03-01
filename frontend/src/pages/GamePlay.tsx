0957aef0c9e96d1c69984e6d59f521662fcfd774
johnkmccann <163747271+johnkmccann@users.noreply.github.com>
2026-03-01 11:26:37 +0100

    Create GamePlay component

---
+++ frontend/src/pages/GamePlay.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const GamePlay: React.FC = () => {
+    return (
+        <div>
+            <h1>Game Play Page</h1>
+            <p>Welcome to the game play page!</p>
+        </div>
+    );
+};
+
+export default GamePlay;
