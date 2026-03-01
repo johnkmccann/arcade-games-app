f6e40341fe2c4097be98dab893f3f5435400ef04
johnkmccann <163747271+johnkmccann@users.noreply.github.com>
2026-03-01 11:26:39 +0100

    Add Home component for the homepage

---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home: React.FC = () => {
+    return (
+        <div>
+            <h1>Welcome to the Arcade Games App!</h1>
+            <p>Explore a variety of games and have fun!</p>
+        </div>
+    );
+};
+
+export default Home;
