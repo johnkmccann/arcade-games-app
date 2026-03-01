d2f44a91e067f1212c671cff76d59b6b5f374d75
johnkmccann <163747271+johnkmccann@users.noreply.github.com>
2026-03-01 11:26:47 +0100

    Add GameBoard component

---
+++ frontend/src/components/GameBoard.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const GameBoard: React.FC = () => {
+    return (
+        <div className="game-board">
+            {/* Game board layout goes here */}
+            <h1>Game Board</h1>
+        </div>
+    );
+};
+
+export default GameBoard;
